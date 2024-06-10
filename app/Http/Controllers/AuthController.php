448df10ff7a08854c1cd8e3c67f0b85b9e5b5d3c
unknown <you@example.com>
2024-06-10 22:15:29 +0200

    add fixed hsoting

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,10 +33,10 @@ class AuthController extends Controller
             $new_name = uniqid() . '.' . $image->getClientOriginalExtension();
             
             // Ensure the directory exists
-            File::ensureDirectoryExists(storage_path('public/storage'));
+            File::ensureDirectoryExists(storage_path('app/public'));
             
             // Move the uploaded file to the destination directory
-            $image->move(storage_path('public/storage/images'), $new_name);
+            $image->move(storage_path('app/public'), $new_name);
         
             // File moved successfully
             // Further processing...
