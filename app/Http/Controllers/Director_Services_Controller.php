9d8bcfa495aebc3dd68a79bb38dd580531304507
unknown <you@example.com>
2024-06-10 23:29:33 +0200

    some changes added succsully

--- app/Http/Controllers/Director_Services_Controller.php
+++ app/Http/Controllers/Director_Services_Controller.php
@@ -111,7 +111,7 @@ class Director_Services_Controller extends Controller
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $new_name = rand().'.'.$image->getClientOriginalExtension();
-            $image->move(storage_path('app/public/images'), $new_name);
+            $image->move(storage_path('/app/public/images'), $new_name);
             return response()->json([
                 'message' => 'image upload successful',
                 'name' => $new_name,
